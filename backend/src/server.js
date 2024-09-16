44cb5ddb385bc878948fae39bd8b6a627531e06a
Pete <petr.stepanenko@gmail.com>
2024-09-16 18:16:00 -0500

    Fix minor formatting and spelling errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,23 @@
 import express from 'express';
 
 const app = express();
-const port = process.ENV.port;
+const port = process.env.PORT;
 
 app.use(express.json());
 
 const workPeriods = [
-    { id: 1, startTime: '2024-08-10T08:00:00Z', endTime: '2024-08-10T16:00:00Z', jobId: 101, userId: 1 },
-    { id: 2, startTime: '2024-08-11T09:00:00Z', endTime: '2024-08-11T17:00:00Z', jobId: 102, userId: 2 }
+  {
+    id: 1, startTime: '2024-08-10T08:00:00Z', endTime: '2024-08-10T16:00:00Z', jobId: 101, userId: 1,
+  },
+  {
+    id: 2, startTime: '2024-08-11T09:00:00Z', endTime: '2024-08-11T17:00:00Z', jobId: 102, userId: 2,
+  },
 ];
 
 app.get('/work_periods', async (req, res) => {
-    res.json(workPeriods);
+  res.json(workPeriods);
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
