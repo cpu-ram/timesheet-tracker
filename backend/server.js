3bb19162b0e6a42b91aa624594d5890f0032c9a3
Pete <petr.stepanenko@gmail.com>
2024-09-16 21:10:57 -0500

    Improve project structure

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,9 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import app from './src/index.js';  // Import app logic from index.js
 
 dotenv.config();
-const app = express();
-const port = process.env.PORT;
 
-app.use(express.json());
-
-const workPeriods = [
-  {
-    id: 1, startTime: '2024-08-10T08:00:00Z', endTime: '2024-08-10T16:00:00Z', jobId: 101, userId: 1,
-  },
-  {
-    id: 2, startTime: '2024-08-11T09:00:00Z', endTime: '2024-08-11T17:00:00Z', jobId: 102, userId: 2,
-  },
-];
-
-app.get('/work-periods', async (req, res) => {
-  res.json(workPeriods);
-});
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,20 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+const workPeriods = [
+  {
+    id: 1, startTime: '2024-08-10T08:00:00Z', endTime: '2024-08-10T16:00:00Z', jobId: 101, userId: 1,
+  },
+  {
+    id: 2, startTime: '2024-08-11T09:00:00Z', endTime: '2024-08-11T17:00:00Z', jobId: 102, userId: 2,
+  },
+];
+
+// Define routes in index.js
+app.get('/work-periods', async (req, res) => {
+  res.json(workPeriods);
 });
 
-app.listen(PORT, () => {
-  console.log('Server is running on port ${PORT}');
-});
+export default app;  // Export the app instance
