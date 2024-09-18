9e930474b8be44bcae0d066f28de8dea1f653be4
Pete <petr.stepanenko@gmail.com>
2024-09-17 19:57:10 -0500

    Fix a minor syntax error

--- backend/src/routes/workPeriodRoutes.js
+++ backend/src/routes/workPeriodRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getWorkPeriods } from '../controllers/workPeriodController.js';
+import getWorkPeriods from '../controllers/workPeriodController.js';
 
 const router = express.Router();
 router.get('/', getWorkPeriods);
