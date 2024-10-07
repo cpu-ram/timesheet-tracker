f409b75068b6968652ab1cbd529d1b46bd25a606
Pete <petr.stepanenko@gmail.com>
2024-10-06 21:55:04 -0500

    Set up the database retrieval for backend/src/repositories/getWorkBlockRecords.js

--- backend/src/repositories/getWorkBlockRecords.js
+++ backend/src/repositories/getWorkBlockRecords.js
@@ -1,15 +1,22 @@
 import pool from '../config/db.js';
+import { format } from 'date-fns';
 
-export const getWorkBlockRecords = (
+export const getWorkBlockRecords = async (
   employeeId,
   startDate,
   endDate,
 ) => {
-  const start = new Date(startDate);
-  const end = new Date(endDate);
-  return workBlockRecords.filter(
-    (element) => element.employeeId === employeeId
-      && new Date(element.startTime) >= start
-      && new Date(element.endTime) <= end,
-  );
+  pgDateFormat = 'yyyy-MM-dd HH:mm:ss';
+  const formattedStartDate = format(startDate, pgDateFormat);
+  const formattedEndDate = format(endDate, pgDateFormat);
+  const query = `
+    SELECT * FROM work_periods
+    WHERE employee_id = $1
+    AND work_start> = #2
+    AND work_end <= $3;
+  `;
+  const values = [employee_id, formattedStartDate, formattedEndDate];
+
+  const result = await pool.query(query, values);
+  return result.rows;
 };
