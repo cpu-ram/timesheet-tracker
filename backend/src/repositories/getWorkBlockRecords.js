1895bfd7b6fd5860f2ca20906210f270b56169b2
Pete <petr.stepanenko@gmail.com>
2024-10-07 20:38:56 -0500

    Stub the database-retrieving getWorkBlockRecords

--- backend/src/repositories/getWorkBlockRecords.js
+++ backend/src/repositories/getWorkBlockRecords.js
@@ -1,22 +1,25 @@
 import pool from '../config/db.js';
 import { format } from 'date-fns';
+import { pgDateFormat } from './utils/pgFormats.js';
 
 export const getWorkBlockRecords = async (
   employeeId,
   startDate,
   endDate,
 ) => {
-  pgDateFormat = 'yyyy-MM-dd HH:mm:ss';
   const formattedStartDate = format(startDate, pgDateFormat);
   const formattedEndDate = format(endDate, pgDateFormat);
   const query = `
-    SELECT * FROM work_periods
+    SELECT * FROM work_intervals
     WHERE employee_id = $1
-    AND work_start> = #2
+    AND work_start >= $2
     AND work_end <= $3;
   `;
-  const values = [employee_id, formattedStartDate, formattedEndDate];
-
-  const result = await pool.query(query, values);
-  return result.rows;
+  const values = [employeeId, formattedStartDate, formattedEndDate];
+  try {
+    const result = await pool.query(query, values);
+    return result.rows;
+  } catch (error) {
+    throw new Error('Unable to fetch work block records');
+  }
 };
