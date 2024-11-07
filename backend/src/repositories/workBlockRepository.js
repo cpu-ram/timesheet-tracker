f173e8a0cfdd555f276751dedc9c3dc7baae052a
Pete <petr.stepanenko@gmail.com>
2024-11-06 21:47:12 -0600

    Clean up imports syntax

--- backend/src/repositories/workBlockRepository.js
+++ backend/src/repositories/workBlockRepository.js
@@ -2,11 +2,11 @@ import addEmployeeRecord from './addEmployeeRecord.js';
 import addProjectRecord from './addProjectRecord.js';
 import { addWorkBlockRecord } from './addWorkBlockRecord.js';
 import { deleteWorkBlockRecord } from './deleteWorkBlockRecord.js';
-import { getDefaultJobsitePropertiesRecord } from './getDefaultJobsitePropertiesRecord.js';
+import getDefaultJobsitePropertiesRecord from './getDefaultJobsitePropertiesRecord.js';
 import { getWorkBlockRecords } from './getWorkBlockRecords.js';
 
 export {
   addEmployeeRecord, addProjectRecord, addWorkBlockRecord,
   deleteWorkBlockRecord, getDefaultJobsitePropertiesRecord,
-  getWorkBlockRecords
+  getWorkBlockRecords,
 };
