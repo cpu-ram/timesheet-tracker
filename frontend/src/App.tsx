82fe6b1855d46197713ee6512830debbae2d2f21
Pete <petr.stepanenko@gmail.com>
2024-12-29 15:43:57 -0600

    fix(frontend): improve styling
    -Adapt for extremely small screen
    
    -Align the calendar display to the top

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 import { User } from './types/userType.tsx';
 import UserSelectorPage from './pages/UserSelectorPage.tsx';
 import TimesheetPage from './pages/TimesheetPage.tsx';
@@ -8,37 +9,39 @@ function App() {
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
 
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            selectedUser ?
-              (<Navigate to="/timesheet" />)
-              :
-              (<Navigate to="/select-user" />)
-          }
-        />
+    <>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              selectedUser ?
+                (<Navigate to="/timesheet" />)
+                :
+                (<Navigate to="/select-user" />)
+            }
+          />
 
-        <Route
-          path="/select-user"
-          element={
-            <UserSelectorPage
-              {...{ selectedUser, setSelectedUser }}
-            />
-          }
-        />
+          <Route
+            path="/select-user"
+            element={
+              <UserSelectorPage
+                {...{ selectedUser, setSelectedUser }}
+              />
+            }
+          />
 
-        <Route
-          path="/timesheet"
-          element={
-            selectedUser ?
-              (<TimesheetPage selectedUser={selectedUser} />) :
-              (<Navigate to="/select-user" />)
-          }
-        />
-      </Routes>
-    </Router>
+          <Route
+            path="/timesheet"
+            element={
+              selectedUser ?
+                (<TimesheetPage selectedUser={selectedUser} />) :
+                (<Navigate to="/select-user" />)
+            }
+          />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -1,38 +1,50 @@
 import { Grid, Typography } from '@mui/material';
 import { startOfWeek, addDays, isSameDay } from 'date-fns';
+import { GlobalStyles } from '@mui/material';
 
 const Calendar = () => {
   const today = new Date();
-  const lastWeekStart = addDays(startOfWeek(today, { weekStartsOn: '1' }), -7);
+  const lastWeekStart = addDays(startOfWeek(today, { weekStartsOn: 1 }), -7);
   const days = Array.from({ length: 14 }).map((_, index) => addDays(lastWeekStart, index));
 
   return (
-    <Grid container spacing={2}>
-      {[0, 7].map((startIndex) => (
-        <Grid item xs='12' key={startIndex}>
-          <Grid container spacing={1} id='month-header'>
-            {days.slice(startIndex, startIndex + 7).map((day, index) => {
-              if (isSameDay(day, lastWeekStart) || day.getDate() === 1) {
-                return (
-                  <Grid item xs='1.5' key={index}>
-                    <Typography>{day.toLocaleString('default', { month: 'short' })}</Typography>
-                  </Grid>
-                )
-              }
-              return (<Grid item xs='1.5' key={index}></Grid>)
-            })}
-          </Grid>
+    <>
+      <GlobalStyles styles={{ body: { placeItems: 'start' } }} />
+      <Grid container spacing={2}>
+        <Grid container item xs={12} key="dayNames">
+          {['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'].map((day, index) => (
+            <Grid item xs={1.71} sm={1.71} key={index} style={{ padding: '8px', display: 'flex', justifyContent: 'center' }}>
+              <Typography variant="h6" align="center">{day}</Typography>
+            </Grid>
+          ))}
+        </Grid>
 
-          <Grid container spacing={1} id='days'>
-            {days.slice(startIndex, startIndex + 7).map((day, index) => (
-              <Grid item xs='1.5' key={index}>
-                <Typography>{day.toLocaleString('default', { day: 'numeric' })}</Typography>
-              </Grid>
-            ))}
+        {[0, 7].map((startIndex) => (
+          <Grid container item xs={12} key={startIndex}>
+            <Grid container spacing={1} key="month-header">
+              {days.slice(startIndex, startIndex + 7).map((day, index) => {
+                if (isSameDay(day, lastWeekStart) || day.getDate() === 1) {
+                  return (
+                    <Grid item xs={1.71} sm={1.71} key={index} style={{ padding: '8px', display: 'flex', justifyContent: 'center' }}>
+                      <Typography variant="h6" align="center">{day.toLocaleString('default', { month: 'short' })}</Typography>
+                    </Grid>
+                  );
+                }
+                return <Grid item xs={1.71} sm={1.71} key={index} style={{ padding: '8px', display: 'flex', justifyContent: 'center' }}></Grid>;
+              })}
+            </Grid>
+
+            <Grid container spacing={1} id="days">
+              {days.slice(startIndex, startIndex + 7).map((day, index) => (
+                <Grid item xs={1.71} sm={1.71} key={index} style={{ padding: '8px', display: 'flex', justifyContent: 'center' }}>
+                  <Typography variant="h6" align="center">{day.toLocaleString('default', { day: 'numeric' })}</Typography>
+                </Grid>
+              ))}
+            </Grid>
           </Grid>
-        </ Grid>
-      ))}
-    </Grid>
+        ))}
+      </Grid>
+    </>
   );
 };
 
