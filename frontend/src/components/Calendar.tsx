2b86823a34ada6c05733c8cc972b95ce52264a64
Pete <petr.stepanenko@gmail.com>
2024-12-29 12:50:08 -0600

    feat(frontend): implement minimally functional calendar display

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -1,23 +1,29 @@
 import { Grid, Typography } from '@mui/material';
-import { startOfWeek, addDays } from 'date-fns';
+import { startOfWeek, addDays, isSameDay } from 'date-fns';
 
 const Calendar = () => {
   const today = new Date();
-  const lastWeekStart = addDays(startOfWeek(today), -7);
+  const lastWeekStart = addDays(startOfWeek(today, { weekStartsOn: '1' }), -7);
   const days = Array.from({ length: 14 }).map((_, index) => addDays(lastWeekStart, index));
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12} key="month-1">
-        <Typography>
-          {
-            lastWeekStart.toLocaleString('default', { month: 'short' })
-          }
-        </Typography>
-      </Grid>
       {[0, 7].map((startIndex) => (
         <Grid item xs='12' key={startIndex}>
-          <Grid container spacing={1}>
+          <Grid container spacing={1} id='month-header'>
+            {days.slice(startIndex, startIndex + 7).map((day, index) => {
+              if (isSameDay(day, lastWeekStart) || day.getDate() === 1) {
+                return (
+                  <Grid item xs='1.5' key={index}>
+                    <Typography>{day.toLocaleString('default', { month: 'short' })}</Typography>
+                  </Grid>
+                )
+              }
+              return (<Grid item xs='1.5' key={index}></Grid>)
+            })}
+          </Grid>
+
+          <Grid container spacing={1} id='days'>
             {days.slice(startIndex, startIndex + 7).map((day, index) => (
               <Grid item xs='1.5' key={index}>
                 <Typography>{day.toLocaleString('default', { day: 'numeric' })}</Typography>
