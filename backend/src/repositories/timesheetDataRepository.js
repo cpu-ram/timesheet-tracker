c47808af03ba86c5c0496a9de516d670c498041f
Pete <petr.stepanenko@gmail.com>
2025-01-01 15:12:51 -0600

    fix(backend): correct repository-level letter case formatting

--- backend/src/repositories/timesheetDataRepository.js
+++ backend/src/repositories/timesheetDataRepository.js
@@ -18,13 +18,13 @@ export async function fetchTimesheetDataRecords(employeeId, from, to) {
   }
   const query = `
     SELECT 
-      projects.project_address as projectAddress,
-      projects.project_name as projectName,
-      projects.project_id as projectId,
-      work_start as workPeriodStart,
-      break_start as breakStart,
-      break_end as breakEnd,
-      work_end as workPeriodEnd
+      projects.project_address as "projectAddress",
+      projects.project_name as "projectName",
+      projects.project_id as "projectId",
+      work_start as "workPeriodStart",
+      break_start as "breakStart",
+      break_end as "breakEnd",
+      work_end as "workPeriodEnd"
     FROM
       work_periods 
     LEFT JOIN 
@@ -32,21 +32,24 @@ export async function fetchTimesheetDataRecords(employeeId, from, to) {
     ON
       work_periods.project_id=projects.project_id
     WHERE
-      work_start >= $1 
+      date >= $1 
     AND
-      work_end <= $2
+      date <= $2
     ;
   `;
   const values = [formattedFrom, formattedTo];
   let result = undefined;
   try {
     result = await dbPool.query(query, values);
+
+    if (result.rowCount === 0) {
+      return null;
+    }
+
+    return result.rows;
   }
   catch (error) {
     throw new Error('Unable to fetch timesheet data records');
   }
-  if (result.rowCount === 0) {
-    return null;
-  }
-  return result.rows;
+
 }
