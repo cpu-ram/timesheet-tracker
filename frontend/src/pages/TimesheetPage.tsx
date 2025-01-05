1c4fddba3cef1a1b282fd0016bd3927a71ead57d
Pete <petr.stepanenko@gmail.com>
2025-01-05 15:33:02 -0600

    refactor(frontend): isolate fetchTimesheetData for better modularity

--- frontend/src/pages/TimesheetPage.tsx
+++ frontend/src/pages/TimesheetPage.tsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from 'react';
 import { Grid, Typography, Box } from '@mui/material';
 import { startOfDay, format } from 'date-fns';
+import { capitalize } from 'lodash';
 import Calendar from '../components/Calendar.tsx';
 import WorkDay from '../components/WorkDay.tsx';
-import { capitalize } from 'lodash';
+import fetchTimesheetData from '../utils/fetchTimesheetData.ts';
+
 
 const TimesheetPage = ({ selectedUser }) => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(startOfDay(new Date()));
   const [workData, setWorkData] = useState([]);
 
-  const fetchTimesheetData = async (dateString) => {
+  /*const fetchTimesheetData = async (dateString) => {
     const baseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
     const timesheetDataSubPath = '/reports/weekly/data';
 
@@ -43,13 +45,19 @@ const TimesheetPage = ({ selectedUser }) => {
       throw new Error(error);
     }
   }
+  */
 
 
   useEffect(() => {
-    if (selectedDate) {
-      fetchTimesheetData(format(selectedDate, 'yyyy-MM-dd'));
-    }
-  }, [selectedDate])
+    const fetchData = async () => {
+      if (selectedDate) {
+        const timesheetData = await fetchTimesheetData({ date: selectedDate, userId: selectedUser.id });
+        setWorkData(timesheetData);
+      }
+    };
+
+    fetchData();
+  }, [selectedDate, selectedUser])
 
   return (
     <div>
---
+++ frontend/src/utils/fetchTimesheetData.ts
@@ -0,0 +1,40 @@
+import { format } from 'date-fns';
+
+const fetchTimesheetData = async ({ date, userId }) => {
+  const dateString = format(date, 'yyyy-MM-dd');
+  const baseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+  const timesheetDataSubPath = '/reports/weekly/data';
+
+
+  try {
+    const response = await fetch(`${baseUrl}${timesheetDataSubPath}?employeeId=${userId}&from=${dateString}&to=${dateString}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch timesheet data');
+    }
+    const data = await response.json();
+
+    const modifiedData =
+      data ?
+        data.map((workBlock) => (
+          {
+            ...workBlock,
+            workBlockStart:
+              workBlock.workBlockStart ? new Date(workBlock.workBlockStart) : null,
+            workBlockEnd:
+              workBlock.workBlockEnd ? new Date(workBlock.workBlockEnd) : null,
+            breakStart:
+              workBlock.breakStart ? new Date(workBlock.breakStart) : null,
+            breakEnd:
+              workBlock.breakEnd ? new Date(workBlock.breakEnd) : null
+          }
+        ))
+        :
+        null;
+    return modifiedData;
+  }
+  catch (error) {
+    throw new Error(error);
+  }
+}
+
+export default fetchTimesheetData;
