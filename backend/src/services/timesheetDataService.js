6c8ba0f784adfb82211768e253a149f04f72b5ea
Pete <petr.stepanenko@gmail.com>
2025-02-03 17:03:30 -0600

    refactor(backend): restructure timesheetData retrurned to user hierarchically by date

--- backend/src/services/timesheetDataService.js
+++ backend/src/services/timesheetDataService.js
@@ -1,15 +1,33 @@
-import { parse, isValid } from 'date-fns';
+import { parse, isValid, differenceInCalendarDays, addDays } from 'date-fns';
 import { fetchTimesheetDataRecords } from '../repositories/timesheetDataRepository.js';
 
 export async function getTimesheetData(employeeId, from, to) {
   let result = undefined;
   try {
-    [from, to] = [from, to].map(date => parse(date, 'yyyy-MM-dd', new Date()));
-    if (!from || !to || !isValid(from) || !isValid(to)) {
+    let [fromDate, toDate] = [from, to].map(date => parse(date, 'yyyy-MM-dd', new Date()));
+    if (!from || !to || !isValid(fromDate) || !isValid(toDate)) {
       throw new Error('Invalid date format');
     }
 
-    result = await fetchTimesheetDataRecords(employeeId, from, to);
+    result = Array.from(
+      { length: differenceInCalendarDays(toDate, fromDate) + 1 },
+      (_, index) => addDays(fromDate, index)
+    ).map(
+      date => ({
+        date: date,
+        workBlocks: [],
+      })
+    );
+
+    let data = await fetchTimesheetDataRecords(employeeId, fromDate, toDate);
+
+    if (data === null) return result;
+
+    for (let record of data) {
+      let recordDate = record.workBlockDate;
+      let dateIndex = differenceInCalendarDays(recordDate, fromDate);
+      result[dateIndex].workBlocks.push(record);
+    }
   }
   catch (error) {
     throw new Error(error);
