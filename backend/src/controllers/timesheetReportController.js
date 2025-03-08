578c80f6fd856c255ac52965f48819096a760d44
Pete <petr.stepanenko@gmail.com>
2025-03-08 12:00:58 -0600

    refactor(backend): implement weeklyReportService as a 'factory'

--- backend/src/controllers/timesheetReportController.js
+++ backend/src/controllers/timesheetReportController.js
@@ -11,26 +11,31 @@ export const generateTimesheetReportHandler = async (req, res) => {
   let resultBuffer = undefined;
 
   const employeeId = req.body.employeeId;
-  const employeeName = await getEmployee(employeeId).name;
+  const employee = await getEmployee(employeeId)
+  const employeeName = employee.name;
   let from = Temporal.PlainDate.from(req.body.from);
   let to = Temporal.PlainDate.from(req.body.to);
-  const reportFileFormat = req.body.reportFileFormat;
-  const contentType = switch (reportFileFormat) {
-    case '.pdf':
-      return 'application/pdf';
-    case '.docx':
-      return 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
-  };
+  const reportFileFormat = req.body.format;
+  const contentType = (() => {
+    switch (reportFileFormat) {
+      case '.pdf':
+        return 'application/pdf';
+      case '.docx':
+        return 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+      default:
+        throw new TypeError('Invalid report file format');
+    }
+  })();
 
   try {
     resultBuffer = await generateWeeklyReport(
-      employeeId, from, to, reportFileFormat, res
+      employeeId, from, to, employeeName, reportFileFormat
     );
 
     res.setHeader('Content-Type', contentType);
 
     const fileName = `${employeeName.replace(/ /g, '_')}_timesheet_${from.toString()}_${to.toString()}`;
-    res.setHeader('Content-Disposition', `attachment; filename='${fileName}.pdf'`);
+    res.setHeader('Content-Disposition', `attachment; filename='${fileName}.${reportFileFormat}'`);
     res.setHeader('X-File-Name', `${fileName}${reportFileFormat}`);
 
     res.status(200).send(resultBuffer);
--- backend/src/services/weeklyReport/weeklyReportService.js
+++ backend/src/services/weeklyReport/weeklyReportService.js
@@ -7,15 +7,13 @@ import { dirname } from 'path';
 import { generateWeeklyReportData } from './generateWeeklyReportData.js';
 import generateHtmlTemplate from './pdf/generateHtmlTemplate.js';
 import convertHtmlToPdf from './pdf/convertHtmlToPdf.js';
-import Docxtemplater from 'docxtemplater';
-import PizzZip from 'pizzip';
-import { getEmployee } from '../services/employeeService.js';
+import { getEmployee } from '../employeeService.js';
 
 
-export default async function generateWeeklyReport(employeeId, from, to, signedName = 'John Doe', reportFileFormat, req, res) {
+export default async function generateWeeklyReport(employeeId, from, to, employeeName = 'John Doe', reportFileFormat) {
 
-  const reportData = await generateWeeklyReportData(employeeId, from, to, signedName);
-  const resultBuffer = undefined;
+  let reportData = await generateWeeklyReportData(employeeId, from, to, employeeName);
+  let resultBuffer = undefined;
 
   switch (reportFileFormat) {
     case '.pdf':
@@ -23,8 +21,20 @@ export default async function generateWeeklyReport(employeeId, from, to, signedN
       resultBuffer = await Buffer.from(await convertHtmlToPdf(htmlTemplate));
       break;
     case '.docx':
+      const filename = fileURLToPath(import.meta.url);
+      const directory = dirname(filename);
+      const templatePath = path.resolve(directory, '../../assets/timesheet-template.docx');
+      const content = fs.readFileSync(templatePath, 'binary');
+      const zip = new PizZip(content);
 
-      return null;
+      const doc = new Docxtemplater(zip);
+      doc.setData(reportData);
+      doc.render();
+
+      resultBuffer = doc.getZip().generate({ type: 'nodebuffer' });
+      break;
+    default:
+      throw new TypeError('Invalid report file format');
   }
 
   return resultBuffer;
