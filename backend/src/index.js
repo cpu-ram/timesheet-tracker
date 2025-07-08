b4557936d9bb16399bb8c78c2b713b0133a696b0
Pete <petr.stepanenko@gmail.com>
2025-07-07 21:21:07 -0500

    fix(backend): fix a bug preventing proper cookie operation during local development

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ app.use(session({
   cookie: {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'none',
+    sameSite: process.env.NODE_ENV==='production'? 'none' : 'lax',
     maxAge: 10 * 24 * 60 * 60 * 1000,
   }
 }));
