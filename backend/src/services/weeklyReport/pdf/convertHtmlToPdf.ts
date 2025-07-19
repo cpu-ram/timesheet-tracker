e6da2fa65c694f6a2f07cfea9be92b0c589f26bc
Petr Stepanenko <petr.stepanenko@gmail.com>
2025-07-19 04:05:59 +0000

    fix(backend): configure puppeteer to prevent  prod crash

--- backend/src/services/weeklyReport/pdf/convertHtmlToPdf.ts
+++ backend/src/services/weeklyReport/pdf/convertHtmlToPdf.ts
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer';
 
 export default async function convertHtmlToPdf(htmlContent: string) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox']});
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
   const pdfBuffer = await page.pdf({ format: 'A4', landscape: true });
